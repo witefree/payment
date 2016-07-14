8d1dae33f216ef73a3ba8f49c4531f9f5191219e
吴佳星 WJX <wujiaxing@chinamobo.com>
2016-07-14 18:47:20 +0800

    commit

--- payment/src/main/java/com/wjx/ehr/attendance/web/AttendanceController.java
+++ payment/src/main/java/com/wjx/ehr/attendance/web/AttendanceController.java
@@ -11,6 +11,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping(value="/attendance")
+/**
+ * 
+ * 版本: [1.0]
+ * 功能说明: 考勤
+ *
+ * 作者: wjx
+ * 创建时间: 2016年7月14日 下午6:46:53
+ */
 public class AttendanceController {
 	
 	@RequestMapping(value = "/list",method = RequestMethod.GET)
